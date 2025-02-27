b123cc81a563703a9eceb44cf7b919c3054e13e7
Pietro Giardina <s233663@dtu.dk>
2025-02-27 18:35:15 +0100

    Finished function to compute likelihood. It's not working

--- various.py
+++ various.py
@@ -6,12 +6,16 @@ from scipy.optimize import minimize, bisect
 import numpy as np
 
 import jax
+from jax import jit
 from jax import vmap
+from jax.scipy.integrate import trapezoid
 from jax.scipy.stats import norm
 import jax.numpy as jnp
 from jaxopt import GradientDescent
+from jaxopt import BFGS
 from jaxopt import Bisection
 
+from timeit import timeit
 import time
 from tqdm import tqdm
 
@@ -35,23 +39,23 @@ def asymm_gaussian_plateau(sigma_l=.7, sigma_r=.2, mu=9.5, plateau_len=3):
 
 #%%
 
-def likelihood_kink(travel_time, t_a, mu_b, sigma):
-    """ Finds the likelihood of a point being a kink minimum,
-    for the travel time, beta and gamma distributions determined by the parameters.
-    Beta and gamma are assumed to be normally distributed.
+def likelihood(travel_time, t_a, mu_b, mu_t, sigma, sigma_t):
+    """ Finds the likelihood of a point realizing a minimum,
+    for the travel time, beta, gamma and t* distributions determined by the parameters.
+    Beta, gamma and t* are assumed to be normally distributed.
     Gamma still has to be implemented
     """
-    def find_bs(beta, travel_time):
+    def find_bs(beta):
         """ Given a travel time function and a beta value,
         finds the interval in which the optimal arrival time is constant
-        (and there are thuss no kink minima).
+        (and there are thus no kink minima).
 
         Returns a couple containing initial and final points of the interval
         """
         
         # A gradient descent algorithm finds the initial point
         in_obj = lambda x: travel_time(x) - beta*x
-        solver = GradientDescent(fun=in_obj, acceleration=False, stepsize=1e-1, maxiter=2500, tol=1e-2)
+        solver = GradientDescent(fun=in_obj, acceleration=False, stepsize=1e-1, maxiter=2500)
         b_i, _ = solver.run(0.)
 
         # The final point is found where the line starting from the initial point,
@@ -64,12 +68,12 @@ def likelihood_kink(travel_time, t_a, mu_b, sigma):
         step = .5
         high = jax.lax.while_loop(lambda a: fin_obj(a) > 0, lambda a: a + step, b_i + step)
         low = high - step
-        b_e = Bisection(fin_obj, low, high, tol=1e-2, check_bracket=False, jit=True).run().params
+        b_e = Bisection(fin_obj, low, high, check_bracket=False, jit=True).run().params
 
         # The interval extremes are returned
         return (b_i, b_e)
     
-    def find_b0(t_a, travel_time):
+    def find_b0(t_a):
         """ Given an arrival time, finds the maximal beta such that
         the arrival time is a kink equilibrium for every value higher than the one returned.
 
@@ -83,7 +87,7 @@ def likelihood_kink(travel_time, t_a, mu_b, sigma):
         # The objective function, an indicator function that shows wether the parameter t_a
         # is in the interval for a given beta, is defined
         isin = lambda x, int: jnp.where(jnp.logical_and((x > int[0]), (x < int[1])), 1, -1)
-        isin_obj = lambda b: isin(t_a, find_bs(b, travel_time))
+        isin_obj = lambda b: isin(t_a, find_bs(b))
 
         # If t_a is not in the interval for the starting points,
         # the starting points are returned themselves.
@@ -94,17 +98,45 @@ def likelihood_kink(travel_time, t_a, mu_b, sigma):
             jnp.logical_and(
                 jnp.logical_not(is_max),
                 jnp.logical_not(is_min)),
-            Bisection(isin_obj, min, max, tol=1e-3, check_bracket=False, jit=True).run().params,
+            Bisection(isin_obj, min, max, check_bracket=False, jit=True).run().params,
             jnp.where(is_max, min, max))
         return sol
-    
-    b0 = find_b0(t_a, travel_time)
-    return 1 - norm.cdf(b0, mu_b, sigma)
 
-def likelihood_internal(travel_time, t_a, mu_b, sigma):
+    # For computing the probability that a point is a kink minimum,
+    # an integral is computed as in the latex.
+    b0 = find_b0(t_a)
+    likelihood_kink = norm.pdf(t_a, mu_t, sigma_t) * (1 - norm.cdf(b0, mu_b, sigma))
+
+    # Now for the internal minimum:
+    # the easiest probability to compute is the probability that
+    # a point is allowed to be an internal minimum for some realization of beta
     travel_time_diff = jax.grad(travel_time)
-    return norm.pdf(travel_time_diff(t_a), mu_b, sigma)
+    prob_allowed = norm.pdf(travel_time_diff(t_a), mu_b, sigma)
+
+    def inner_int(b):
+        bs = find_bs(b)
+        return (norm.cdf(bs[1], mu_t, sigma_t) - norm.cdf(bs[0], mu_t, sigma_t)) * norm.pdf(b, mu_b, sigma)
     
+    min = 1e-2
+    x = jnp.linspace(min, 1-min, 50)
+    fx = vmap(inner_int)(x)
+    int_result = trapezoid(fx, x)
+
+    likelihood_internal = int_result * prob_allowed
+
+    return likelihood_kink + likelihood_internal
+
+def total_liks(travel_time, t_as):
+    def mapped_lik(mu_b, mu_t, sigma, sigma_t):
+        lik_restr = lambda t_a: likelihood(travel_time, t_a, mu_b, mu_t, sigma, sigma_t)
+        return vmap(lik_restr)(t_as)
+    return mapped_lik
+
+def total_log_lik(travel_time, t_as):
+    def mapped_lik(mu_b, mu_t, sigma, sigma_t):
+        lik_restr = lambda t_a: likelihood(travel_time, t_a, mu_b, mu_t, sigma, sigma_t)
+        return jnp.sum(jnp.log(vmap(lik_restr)(t_as)))
+    return mapped_lik
 #%%
 
 beta = .7
@@ -118,25 +150,32 @@ plt.plot(ts, find_td(asymm_gaussian_plateau())(betas, gammas, ts), '.')
 plt.show()
 
 #%%
-num=200
-betas, gammas, ts, t_as = generate_arrival(num, travel_time=asymm_gaussian_plateau())
-#%%
-liks = np.array([likelihood_kink(asymm_gaussian_plateau(), t_a, .7, .1) for t_a in tqdm(t_as)])
-#%%
-
-lik_int_restr = lambda t: likelihood_internal(asymm_gaussian_plateau(), t, .7, .1)
-liks_int = vmap(lik_int_restr)(t_as)
+num=1000
+betas, gammas, ts, t_as = generate_arrival(num, travel_time=asymm_gaussian_plateau(), mu_gamma=1000)
+lik_fun = jit(lambda mus: -total_log_lik(asymm_gaussian_plateau(), t_as)(mus[0], mus[1], mus[2], mus[3]))
 
 #%%
-liks_restr = lambda t: likelihood_kink(asymm_gaussian_plateau(), t, .7, .1)
-start_time = time.time()
-liks_new = vmap(liks_restr)(t_as)
-total_time = time.time() - start_time
-print(f"{total_time/num:.3} s per iteration, {total_time:.3} s in total")
+
+res = minimize(lik_fun, (10, 10, .5, .5), method="Nelder-Mead")
+print(res.x)
 #%%
 
+solver = GradientDescent(lik_fun, verbose=True)
+init = (10., 10., .5, .5)
+val, state = solver.run(init)
+print("finished optimizing")
+print(val)
+#%%
+
+tot_liks = vmap(lambda t: likelihood_kink(asymm_gaussian_plateau(), t, .7, 9.5, .1, 1.) + likelihood_internal(asymm_gaussian_plateau(), t, .7, 9.5, .1, 1.))
+liks = tot_liks(t_as)
+#%%
+
+liks = total_liks(asymm_gaussian_plateau(), t_as)(.7, 9.5, .1, 1.)
+
+#%%
 fig, axs = plt.subplots(1, 2)
-colors = cm.plasma(liks_int)
+colors = cm.plasma(liks/max(liks))
 axs[0].scatter(np.random.normal(size=num), t_as, s=10, color=colors)
 axs[1].scatter(ts, t_as, s=10, color=colors)
 fig.show()
