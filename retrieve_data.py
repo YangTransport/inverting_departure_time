2fb37467d6c9ea2b5fb229595211fe0c33ebc994
Pietro Giardina <s233663@dtu.dk>
2025-03-05 18:08:47 +0100

    Renamed jax's norm and truncnorm to jnorm, jtruncnorm

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,6 +1,7 @@
 from jax import grad, vmap
 from jax.scipy.integrate import trapezoid
-from jax.scipy.stats import truncnorm, norm
+from jax.scipy.stats import truncnorm as jtruncnorm
+from jax.scipy.stats import norm as jnorm
 import jax.numpy as jnp
 
 from find_points import find_bs, find_gs, find_b0, find_g0
@@ -14,20 +15,20 @@ def likelihood(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t):
     """
 
     # The truncated normals pdf and cdf are definde here
-    cdf_b = lambda b: norm.cdf(b, mu_b, sigma)
-    cdf_g = lambda g: norm.cdf(g, mu_g, sigma)
-    pdf_b = lambda b: norm.pdf(b, mu_b, sigma)
-    pdf_g = lambda g: norm.pdf(g, mu_g, sigma)
-    # cdf_b = lambda b: truncnorm.cdf(b, -mu_b / sigma, (1 - mu_b) / sigma, loc=mu_b, scale=sigma)
-    # cdf_g = lambda g: truncnorm.cdf(g, (1 -mu_g) / sigma, 100000, loc=mu_g, scale=sigma)
-    # pdf_b = lambda b: truncnorm.pdf(b, -mu_b / sigma, (1 - mu_b) / sigma, mu_b, sigma)
-    # pdf_g = lambda g: truncnorm.pdf(g, (1 - mu_g) / sigma, 10000, mu_g, sigma)
+    cdf_b = lambda b: jnorm.cdf(b, mu_b, sigma)
+    cdf_g = lambda g: jnorm.cdf(g, mu_g, sigma)
+    pdf_b = lambda b: jnorm.pdf(b, mu_b, sigma)
+    pdf_g = lambda g: jnorm.pdf(g, mu_g, sigma)
+    # cdf_b = lambda b: truncjnorm.cdf(b, -mu_b / sigma, (1 - mu_b) / sigma, loc=mu_b, scale=sigma)
+    # cdf_g = lambda g: truncjnorm.cdf(g, (1 -mu_g) / sigma, 100000, loc=mu_g, scale=sigma)
+    # pdf_b = lambda b: truncjnorm.pdf(b, -mu_b / sigma, (1 - mu_b) / sigma, mu_b, sigma)
+    # pdf_g = lambda g: truncjnorm.pdf(g, (1 - mu_g) / sigma, 10000, mu_g, sigma)
 
     # For computing the probability that a point is a kink minimum, an
     # integral is computed as in the latex.
     b0 = find_b0(t_a, travel_time)
     g0 = find_g0(t_a, travel_time)
-    likelihood_kink = norm.pdf(t_a, mu_t, sigma_t) * (1 - cdf_b(b0)) * (1 - cdf_g(g0))
+    likelihood_kink = jnorm.pdf(t_a, mu_t, sigma_t) * (1 - cdf_b(b0)) * (1 - cdf_g(g0))
 
     # Now for the internal minima: the easiest probability to compute
     # is the probability that a point is allowed to be an internal
@@ -42,11 +43,11 @@ def likelihood(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t):
     # interval times the probability of t* being in that interval.
     def inner_int_b(b):
         bs = find_bs(b, travel_time)
-        return (norm.cdf(bs[1], mu_t, sigma_t) - norm.cdf(bs[0], mu_t, sigma_t)) * pdf_b(b)
+        return (jnorm.cdf(bs[1], mu_t, sigma_t) - jnorm.cdf(bs[0], mu_t, sigma_t)) * pdf_b(b)
     
     def inner_int_g(g):
         gs = find_gs(g, travel_time)
-        return (norm.cdf(gs[1], mu_t, sigma_t) - norm.cdf(gs[0], mu_t, sigma_t)) * pdf_g(g)
+        return (jnorm.cdf(gs[1], mu_t, sigma_t) - jnorm.cdf(gs[0], mu_t, sigma_t)) * pdf_g(g)
     
     min_b = 1e-2
     points = 50
@@ -59,7 +60,6 @@ def likelihood(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t):
     x_g = jnp.linspace(min_g, max_g, points)
     fx_g = vmap(inner_int_g)(x_g)
     int_result_g = trapezoid(fx_g, x_g)
-
     likelihood_internal = int_result_b * prob_allowed_b + int_result_g * prob_allowed_g
     return likelihood_kink + likelihood_internal
 
