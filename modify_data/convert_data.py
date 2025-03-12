d7089e1b96aa8b23d6eeb57029fa8959ac849604
Pietro Giardina <s233663@dtu.dk>
2025-03-12 11:44:41 +0100

    Plotting transformed travel time (still has to be smoothed out)

--- modify_data/convert_data.py
+++ modify_data/convert_data.py
@@ -4,16 +4,19 @@ from instant_to_tt import arrival_time
 from load_data import load_data
 
 x, travel_times = load_data()
-
 #%%
-d_travel_times = (travel_times[:-1] - travel_times[1:]) / (x[1] - x[0])
-plt.plot(x[1:], d_travel_times)
-plt.hlines([0.6, -1.4], x[0], x[-1], 'r')
-plt.show()
-
-#%%
-new_tt = np.array([arrival_time(d, 2, x, 1/travel_times) for d in x]) - x
+distance = 2
+new_tt = np.array([arrival_time(d, distance, x, 1/travel_times) for d in x]) - x
 
 plt.plot(x, new_tt)
 plt.plot(x, travel_times)
 plt.show()
+
+#%%
+d_travel_times = (travel_times[:-1] - travel_times[1:]) / (x[:-1] - x[1:])
+d_new_tt = (new_tt[:-1] - new_tt[1:]) / (x[:-1] - x[1:])
+plt.plot(x[1:], d_travel_times)
+plt.plot(x[1:], d_new_tt)
+plt.hlines([0.6, -1.4], x[0], x[-1], 'r')
+plt.show()
+
