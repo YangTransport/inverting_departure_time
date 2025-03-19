20f06a77c7ca36aa11765293d153af3db84dd402
Pietro Giardina <s233663@dtu.dk>
2025-03-19 16:52:56 +0100

    Small changes

--- plot_slides.py
+++ plot_slides.py
@@ -15,7 +15,7 @@ tt = TravelTime(asymm_gaussian_plateau(sigma_l=.7, sigma_r=.4, plateau_len=.4))
 
 num = 1000
 par = [.8, 1.4, 9.5, .3, 1]
-np.random.seed(14)
+np.random.seed(144)
 betas, gammas, ts, t_as = generate_arrival(num, tt, *par)
 
 bs = find_bs(par[0], tt)
@@ -53,18 +53,21 @@ n, bins, patches = ax_bin.hist(t_as, 80)
 ax_bin.set_yticks([])
 ax_bin.set_xlabel(r"$t_a$ (h)")
 
+def_color = patches[0].get_facecolor()
+
 for b, p in zip(bins, patches):
     if b < b_high and b > b_low - p.get_width():
         p.set_facecolor(early_color)
     if b < g_high and b > g_low - p.get_width():
         p.set_facecolor(late_color)
 
-x = np.linspace(6, 13, 300)
+x = np.linspace(6.5, 12.5, 300)
 tt_line = ax_bin.plot(x, tt.f(x)*40, color=tt_color, linewidth=2, label="Travel time function")
 
-labels = [tt_line[0].get_label(), "Early arrivals", "Late arrivals"]
-handles = [tt_line[0], Patch(facecolor=early_color), Patch(facecolor=late_color)]
+labels = [tt_line[0].get_label(), "Early arrivals", "Late arrivals", "On time arrivals"]
+handles = [tt_line[0], Patch(facecolor=early_color), Patch(facecolor=late_color), Patch(facecolor=def_color)]
 ax_bin.legend(handles, labels)
+ax_bin.set_xlim(6.5, 12.5)
 fig_bin.savefig("slides/img/t_as_bins_tt.png", dpi=600)
 plt.close(fig_bin)
 
@@ -72,7 +75,7 @@ plt.close(fig_bin)
 h_len = .4
 arc_len = .3
 text_dist = .15
-x = np.linspace(6, 13, 300)
+x = np.linspace(6.5, 12.5, 300)
 fig_tt, ax_tt = plt.subplots(figsize=(7, 4))
 ax_tt.plot(x, tt.f(x), linewidth=2, color=tt_color, label='Travel time')
 
@@ -122,7 +125,7 @@ ax_ll.set_xlim(6, 12.5)
 ax_ll.legend()
 ax_ll.set_yticks([])
 ax_ll.set_xlabel(r"$t_a$ (h)")
-shade = ax_ll.fill_between(x, lx*4300, color="orange", alpha=.4, label=r'Likelihood of $t_a$')
+shade = ax_ll.fill_between(x, lx*4000, color="orange", alpha=.4, label=r'Likelihood of $t_a$')
 shade.set_visible(False)
 fig_ll.savefig("slides/img/hist_no_ll.png", dpi=600)
 shade.set_visible(True)
@@ -131,11 +134,12 @@ fig_ll.savefig("slides/img/hist_ll.png", dpi=600)
 plt.close(fig_ll)
 
 #%%
+num_ll = 1000
 # par = (.4, 1.3, 9.5, .3, 1)
 par[3] = .3
-_, _, _, t_as = generate_arrival(num, tt, *par)
+_, _, _, t_as_ll = generate_arrival(num_ll, tt, *par)
 
-ll = lambda x, y: total_log_lik(tt, t_as)(x, y, *par[2:])
+ll = lambda x, y: total_log_lik(tt, t_as_ll)(x, y, *par[2:])
 betas_contour =jnp.linspace(.01, .99, 200)
 gammas_contour = jnp.linspace(1.01, 4, 200)
 matrix_ll = vmap(vmap(ll, (0, None)), (None, 0))(betas_contour, gammas_contour) # vmap(ll_actual)(*m_contour)
@@ -145,7 +149,7 @@ matrix_ll = vmap(vmap(ll, (0, None)), (None, 0))(betas_contour, gammas_contour) 
 fig_ct, ax_ct = plt.subplots(figsize=(6, 4))
 X, Y = jnp.meshgrid(betas_contour, gammas_contour)
 ax_ct.contour(betas_contour, gammas_contour, matrix_ll, levels=50)
-ax_ct.plot(par[0], par[1], 'or')
+# ax_ct.plot(par[0], par[1], 'or')
 ax_ct.set_xlabel(r"$\mu_\beta$")
 ax_ct.set_ylabel(r"$\mu_\gamma$")
 fig_ct.savefig("slides/img/contour_beautiful.png", dpi=600)
@@ -153,9 +157,9 @@ fig_ct.savefig("slides/img/contour_beautiful.png", dpi=600)
 #%%
 # par = (.4, 1.3, 9.5, .03, 1)
 par[3]=.03
-_, _, _, t_as = generate_arrival(1000, tt, *par)
+_, _, _, t_as_ll = generate_arrival(num_ll, tt, *par)
 
-ll = lambda x, y: total_log_lik(tt, t_as)(x, y, *par[2:])
+ll = lambda x, y: total_log_lik(tt, t_as_ll)(x, y, *par[2:])
 betas_contour =jnp.linspace(.01, .99, 200)
 gammas_contour = jnp.linspace(1.01, 4, 200)
 matrix_ll = vmap(vmap(ll, (0, None)), (None, 0))(betas_contour, gammas_contour) # vmap(ll_actual)(*m_contour)
